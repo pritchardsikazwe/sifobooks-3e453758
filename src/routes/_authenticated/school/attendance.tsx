92e1fcfc07c50f1e68ba82e113888ae2706c5fcf
pritchardsikazwe <51705904+pritchardsikazwe@users.noreply.github.com>
2026-09-09 18:26:50 +0200

    Connect school attendance screen to ERP service

--- src/routes/_authenticated/school/attendance.tsx
+++ src/routes/_authenticated/school/attendance.tsx
@@ -1,3 +1,3 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { SifoIndustryWorkspace } from "@/components/sifo/SifoIndustryWorkspace";
-export const Route = createFileRoute("/_authenticated/school/attendance")({ component: () => <SifoIndustryWorkspace kind="school" screen="/school/attendance" /> });
+import { SifoSectorConnectedWorkspace } from "@/components/sifo/SifoSectorConnectedWorkspace";
+export const Route = createFileRoute("/_authenticated/school/attendance")({ component: () => <SifoSectorConnectedWorkspace kind="school" screen="/school/attendance" /> });
