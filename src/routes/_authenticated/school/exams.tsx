123be1d07bb7731c3824b791a621049868529dda
pritchardsikazwe <51705904+pritchardsikazwe@users.noreply.github.com>
2026-09-09 18:04:32 +0200

    Align school exams with operational workspace

--- src/routes/_authenticated/school/exams.tsx
+++ src/routes/_authenticated/school/exams.tsx
@@ -1,4 +1,4 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { SifoSectorWorkflow } from "@/components/sifo/SifoSectorWorkflow";
-export const Route = createFileRoute("/_authenticated/school/exams")({ component: RouteComponent });
-function RouteComponent(){ return <SifoSectorWorkflow kind="school" screen="/school/exams"/>; }
+import { SifoIndustryWorkspace } from "@/components/sifo/SifoIndustryWorkspace";
+export const Route = createFileRoute("/_authenticated/school/exams")({ component: SchoolExams });
+function SchoolExams() { return <SifoIndustryWorkspace kind="school" screen="/school/exams" />; }
