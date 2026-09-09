d9015f574f8ac5d8c26482afa5cd37e4261a5a05
pritchardsikazwe <51705904+pritchardsikazwe@users.noreply.github.com>
2026-09-09 18:27:22 +0200

    Connect school fees screen to ERP service

--- src/routes/_authenticated/school/fees.tsx
+++ src/routes/_authenticated/school/fees.tsx
@@ -1,3 +1,3 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { SifoIndustryWorkspace } from "@/components/sifo/SifoIndustryWorkspace";
-export const Route = createFileRoute("/_authenticated/school/fees")({ component: () => <SifoIndustryWorkspace kind="school" screen="/school/fees" /> });
+import { SifoSectorConnectedWorkspace } from "@/components/sifo/SifoSectorConnectedWorkspace";
+export const Route = createFileRoute("/_authenticated/school/fees")({ component: () => <SifoSectorConnectedWorkspace kind="school" screen="/school/fees" /> });
