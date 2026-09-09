338f49f268f4b7891ac5b17be1ec61c7cb305321
pritchardsikazwe <51705904+pritchardsikazwe@users.noreply.github.com>
2026-09-09 17:58:48 +0200

    Clean duplicate hotel reservations route

--- src/routes/_authenticated/hotel-reservations.tsx
+++
@@ -1,3 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-import { SifoIndustryWorkspace } from "@/components/sifo/SifoIndustryWorkspace";
-export const Route = createFileRoute("/_authenticated/hotel-reservations")({ component: () => <SifoIndustryWorkspace kind="hotel" screen="/hotel/reservations" /> });
