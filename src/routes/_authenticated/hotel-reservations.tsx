dfbb028847344b7c4ff90abe793cdc791d59bcf9
pritchardsikazwe <51705904+pritchardsikazwe@users.noreply.github.com>
2026-09-09 17:51:15 +0200

    feat: add hotel reservations workspace

---
+++ src/routes/_authenticated/hotel-reservations.tsx
@@ -0,0 +1,3 @@
+import { createFileRoute } from "@tanstack/react-router";
+import { SifoIndustryWorkspace } from "@/components/sifo/SifoIndustryWorkspace";
+export const Route = createFileRoute("/_authenticated/hotel-reservations")({ component: () => <SifoIndustryWorkspace kind="hotel" screen="/hotel/reservations" /> });
