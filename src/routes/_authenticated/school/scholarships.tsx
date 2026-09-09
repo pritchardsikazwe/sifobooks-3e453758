46be3dd10577d7dbd2bff4749935594bbd38e40d
pritchardsikazwe <51705904+pritchardsikazwe@users.noreply.github.com>
2026-09-09 18:13:33 +0200

    Add school scholarships workspace

---
+++ src/routes/_authenticated/school/scholarships.tsx
@@ -0,0 +1,4 @@
+import { createFileRoute } from "@tanstack/react-router";
+import { SifoIndustryWorkspace } from "@/components/sifo/SifoIndustryWorkspace";
+export const Route = createFileRoute("/_authenticated/school/scholarships")({ component: SchoolScholarships });
+function SchoolScholarships() { return <SifoIndustryWorkspace kind="school" screen="/school/fees" />; }
