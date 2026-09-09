cde0f967610aad6d9145cb66c00bea58e8264d78
pritchardsikazwe <51705904+pritchardsikazwe@users.noreply.github.com>
2026-09-09 18:27:06 +0200

    Connect school examinations screen to ERP service

--- src/routes/_authenticated/school/exams.tsx
+++ src/routes/_authenticated/school/exams.tsx
@@ -1,4 +1,3 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { SifoIndustryWorkspace } from "@/components/sifo/SifoIndustryWorkspace";
-export const Route = createFileRoute("/_authenticated/school/exams")({ component: SchoolExams });
-function SchoolExams() { return <SifoIndustryWorkspace kind="school" screen="/school/exams" />; }
+import { SifoSectorConnectedWorkspace } from "@/components/sifo/SifoSectorConnectedWorkspace";
+export const Route = createFileRoute("/_authenticated/school/exams")({ component: () => <SifoSectorConnectedWorkspace kind="school" screen="/school/exams" /> });
