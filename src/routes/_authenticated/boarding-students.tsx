ea82699370448af4ec6c2ad5be271530245ba6cb
pritchardsikazwe <51705904+pritchardsikazwe@users.noreply.github.com>
2026-09-23 12:54:38 +0200

    feat: add boarding students management page

---
+++ src/routes/_authenticated/boarding-students.tsx
@@ -0,0 +1,6 @@
+import { createFileRoute } from "@tanstack/react-router";
+import { BoardingHouseWorkspace } from "@/components/industry/BoardingHouseWorkspace";
+
+export const Route = createFileRoute("/_authenticated/boarding-students")({
+  component: () => <BoardingHouseWorkspace initialTab="students" />,
+});
