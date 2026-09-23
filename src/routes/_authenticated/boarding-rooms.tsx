94a42a59dbb873ae07d215684738538ca339116c
pritchardsikazwe <51705904+pritchardsikazwe@users.noreply.github.com>
2026-09-23 12:54:31 +0200

    feat: add rooms & beds management page

---
+++ src/routes/_authenticated/boarding-rooms.tsx
@@ -0,0 +1,6 @@
+import { createFileRoute } from "@tanstack/react-router";
+import { BoardingHouseWorkspace } from "@/components/industry/BoardingHouseWorkspace";
+
+export const Route = createFileRoute("/_authenticated/boarding-rooms")({
+  component: () => <BoardingHouseWorkspace initialTab="rooms" />,
+});
